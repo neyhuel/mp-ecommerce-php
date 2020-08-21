e7a5dc52cf34a9d6d92ed934a09f11b4d9308b2e
Victor Nahuel Martinez <nahue.martinez1987@gmail.com>
2020-08-21 17:47:06 -0300

    [Fix] webhook

--- webhook.php
+++ webhook.php
@@ -5,7 +5,7 @@ ini_set('display_errors',1);
 
 $json = $_POST; //json need to be data
 $info = json_encode($json);
-echo ($info);
+var_dump($info);
 $file = fopen('test.json','w+') or die("File not found");
 fwrite($file, $info);
 fclose($file);
