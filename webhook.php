8d38c3eaf2196785b1189eb118b055b693a60640
Victor Nahuel Martinez <nahue.martinez1987@gmail.com>
2020-08-21 17:54:13 -0300

    [Fix] webhook

--- webhook.php
+++ webhook.php
@@ -3,9 +3,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 
-$json = $_POST; //json need to be data
-$info = json_encode($json);
-var_dump($info);
+//$json = $_POST; //json need to be data
+//$info = json_encode($json);
+$info = json_decode(file_get_contents('php://input'), true);
+print_r($info);
 $file = fopen('test.json','w+') or die("File not found");
 fwrite($file, $info);
 fclose($file);
