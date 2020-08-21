1aea4d690b83b7b40d5a8178bc2a90ccf4a93bb6
Victor Nahuel Martinez <nahue.martinez1987@gmail.com>
2020-08-21 20:29:49 -0300

    [Fix] ultimando detalles

--- detail.php
+++ detail.php
@@ -7,6 +7,7 @@ try {
 
   // Agrega credenciales
   MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+  MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
   // Crea un objeto de preferencia
   $preference = new MercadoPago\Preference();
@@ -73,10 +74,6 @@ try {
     echo ($e);
     echo ("</p>");
 }
-
-  echo ('<p style="display: block;position: absolute;z-index: 100;">');
-  echo('https://ashlamir-mp-commerce-php.herokuapp.com' . ltrim($_POST['img'], $_POST['img'][0]));
-  echo ("</p>");
 ?>
 
 
@@ -93,7 +90,7 @@ try {
     src="https://code.jquery.com/jquery-3.4.1.min.js"
     integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
     crossorigin="anonymous"></script>
-    <script src="https://www.mercadopago.com/v2/security.js" view="detail"></script>
+    <script src="https://www.mercadopago.com/v2/security.js" view="item"></script>
 
 
   <link rel="stylesheet" href="./assets/category-landing.css" media="screen, print">
@@ -215,13 +212,6 @@ try {
                                         </h3>
                                     </div>
                                     <a class="mercadopago-button" href="<?php echo $preference->init_point; ?>" style="text-decoration: none !important; padding: 1em 1.7142857142857142em;">Pagar la compra</a>
-<!--                                    <form action="/procesar-pago" method="POST">-->
-<!--                                      <script-->
-<!--                                        src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"-->
-<!--                                        data-button-label="Pagar la compra"-->
-<!--                                        data-preference-id="--><?php //echo $preference->id; ?><!--">-->
-<!--                                      </script>-->
-<!--                                    </form>-->
                                 </div>
                             </div>
                         </div>
--- files.php
+++
@@ -1,3 +0,0 @@
-<?php
-print_r(scandir('/app', 1));
-?>
